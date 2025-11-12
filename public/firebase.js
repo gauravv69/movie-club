910696928d2342a7953310cfdc2569c48a829e81
Gaurav Kadam <109155851+gauravv69@users.noreply.github.com>
2025-11-12 11:23:45 +0000

    Added Firebase Database

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANMbzzi8CseDyrzdROGDkx3qhHnlD8krs",
+  authDomain: "movie-club-app-60152.firebaseapp.com",
+  projectId: "movie-club-app-60152",
+  storageBucket: "movie-club-app-60152.firebasestorage.app",
+  messagingSenderId: "969094939843",
+  appId: "1:969094939843:web:4ccc55a87b3fddc92ece76"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
